a7605d40a5906b830a8dabd7f89741a19fe1393e
cj9806 <cthepyro@gmail.com>
2020-10-29 13:00:02 -0700

    Initial commit

---
+++ C# Unit Test - Student Copy/GraphicsTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using static Raylib_cs.Raylib;  // core methods (InitWindow, BeginDrawing())
+using static Raylib_cs.Color;   // color (RAYWHITE, MAROON, etc.)
+using MathClasses;
+
+namespace GraphicsTest
+{
+    public class core_basic_window
+    {
+        public static int Main()
+        {
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            const int screenWidth = 1600;
+            const int screenHeight = 900;
+
+            InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+            SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
+            {
+                // Update
+                //----------------------------------------------------------------------------------
+                // TODO: Update your variables here
+                //----------------------------------------------------------------------------------
+
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+
+                ClearBackground(BROWN);
+
+                
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            CloseWindow();        // Close window and OpenGL context
+            //--------------------------------------------------------------------------------------
+
+            return 0;
+        }
+    }
+}
